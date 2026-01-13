a1d2aa04f3e9504261f3a5a34f36fdcc718f6ca5
zitriss <zitrissonline@gmail.com>
2026-01-13 18:40:32 +0100

    init

---
+++ ex00/includes/Animal.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 18:12:42 by tlize             #+#    #+#             */
+/*   Updated: 2026/01/13 18:22:29 by tlize            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+public:
+	Animal();
+	Animal(const std::string &type);
+	Animal(const Animal &old_animal);
+	Animal &operator=(const Animal &old_animal);
+	~Animal();
+protected:
+	std::string type;	
+};
+
+#endif
---
+++ ex00/src/Animal.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 18:12:35 by tlize             #+#    #+#             */
+/*   Updated: 2026/01/13 18:40:13 by tlize            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+	this->type = "notype";
+	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(const std::string &type)
+{
+	this->type = type;
+	std::cout << "Animal constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &old_animal)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+	*this = old_animal;
+}
+
+Animal &Animal::operator=(const Animal &old_animal)
+{
+	std::cout << "Animal copy assignement operator called" << std::endl;
+	if (this == &old_animal)
+		return *this;
+	this->type = old_animal.type;
+	return *this;
+}
+
+Animal::~Animal(void)
+{
+	std::cout <<"Animal Destructor called" << std::endl;
+}
